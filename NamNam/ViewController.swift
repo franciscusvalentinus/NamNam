775db44e165c788228ac2ed95d121c8d764852dc
Franciscus Valentinus Ongkosianbhadra <fvalentinus@student.ciputra.ac.id>
2022-04-14 23:53:57 +0700

    done

---
+++ NamNam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NamNam
+//
+//  Created by Franciscus Valentinus Ongkosianbhadra on 14/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
